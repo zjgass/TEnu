c4996fc8f6b3f1ed9a09117cb82f0c348b245c16
Mike Satler <mts5005@gmail.com>
2021-04-05 16:05:25 -0400

    Plan Controller, DAO, Model Setup

---
+++ API/Capstone/DAO/IPlanDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+
+namespace Capstone.DAO
+{
+    public interface IPlanDAO
+    {
+
+        Plan GetPlan(int planId);
+
+        Plan CreatePlan(Plan plan);
+
+        Plan UpdatePlan(Plan plan);
+
+        List<Plan> GetPlans(int userId);
+
+
+    }
+}
