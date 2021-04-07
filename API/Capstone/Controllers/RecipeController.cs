c26e27146fba38546c49357ec5e2f8c9ed698214
Mike Satler <mts5005@gmail.com>
2021-04-07 10:38:34 -0400

    Recipe Controller work, changed meal_plan to mplan everywhere

--- API/Capstone/Controllers/RecipeController.cs
+++ API/Capstone/Controllers/RecipeController.cs
@@ -5,15 +5,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Capstone.Models;
 using Capstone.DAO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Capstone.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RecipeController :Controller
+    public class RecipeController : Controller
     {
         private readonly IRecipeDAO recipeDAO;
 
+        public RecipeController(IRecipeDAO _recipeDAO)
+        {
+            recipeDAO = _recipeDAO;
+        }
+
+        //GetAllRecipes of current user
+        //TODO needs completed, grab userId from jwt 
+        [HttpGet]
+        [Authorize]
+        public ActionResult<List<Recipe>> GetUserRecipes()
+        {
+            int userId = Int32.Parse(User.FindFirst("sub").Value);
+            return Ok(recipeDAO.GetRecipes(userId));    
+        }
+       
         //GetRecipe
         [HttpGet("{recipeId}")]
         public ActionResult<Recipe> GetRecipe(int recipeId)
@@ -34,8 +52,41 @@ namespace Capstone.Controllers
             Recipe updated = recipeDAO.UpdateRecipe(recipe);
             return Ok(updated);
         }
-        //TODO Delete
+        //Remove Recipe
+        [HttpDelete("{recipeId}")]
+        public ActionResult RemoveRecipe(int recipeId)
+        {
+            if(recipeDAO.GetRecipe(recipeId) != null)
+            {
+                if (recipeDAO.DeleteRecipe(recipeId))
+                {
+                    return NoContent();
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+            return BadRequest();
+        }
 
-        //TODO get api/recipe return all recipes of user
+        //Get all public recipes
+        [HttpGet("public")]
+        public ActionResult<List<Recipe>> GetPublicRecipes()
+        {
+            //TODO do we need any error handling here or is it handled by DAO correctly?
+            return Ok(recipeDAO.GetPublicRecipes());
+        }
+
+        //Get recipes by search
+        [HttpGet("search")]
+        public ActionResult<List<Recipe>> SearchRecipes(HttpContext context)
+        {
+            string requestBody = context.Request.Body.ToString();
+            string[] searchArgs = JsonConvert.DeserializeObject<string[]>(requestBody);
+
+            return Ok(recipeDAO.SearchRecipes(searchArgs, true));
+        }
+        
     }
 }
