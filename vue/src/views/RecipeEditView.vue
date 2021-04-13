814c7dc05d85622911757d1555ac0d34da70696e
Zachary <zjgass@gmail.com>
2021-04-13 11:34:49 -0400

    Strange 'Maximum Call Stack Exceeded' error.

---
+++ vue/src/views/RecipeEditView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <recipe-edit/>
+  </div>
+</template>
+
+<script>
+import RecipeEdit from "@/components/RecipeEdit.vue";
+
+export default {
+    name: 'RecipeEdit',
+    components: {
+    RecipeEdit
+  }
+};
+</script>
