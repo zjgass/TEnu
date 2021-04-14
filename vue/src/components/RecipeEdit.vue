111e033807008db1dc082daa7685f56fbe9d7cec
Zachary <zjgass@gmail.com>
2021-04-14 11:02:34 -0400

    Small touch up to recipe edit.

--- vue/src/components/RecipeEdit.vue
+++ vue/src/components/RecipeEdit.vue
@@ -24,15 +24,24 @@
                 <input class="input-integer-text" type='text' v-model="newIngredient.qty" />
                 <h3 class="input-label">  Measurement: </h3>
                 <select class="input-select-dropdown" v-model="newIngredient.unit">
-                    <option value="cups"> cups </option>
-                    <option value="ea"> ea </option>
-                    <option value="g"> g </option>
-                    <option value="mg"> mg </option>
-                    <option value="ml"> ml </option>
-                    <option value="oz"> oz </option>
-                    <option value="qt"> qt </option>
-                    <option value="tbs"> tbs </option>
+                    <option value="pinch"> pinch </option>
                     <option value="tsp"> tsp </option>
+                    <option value="tbs"> tbs </option>
+                    <option value="cups"> cups </option>
+                    <option value="oz"> oz </option>
+                    <option value="lb"> lb </option>
+                    <option value="fl oz"> fl oz </option>
+                    <option value="pt"> pt </option>
+                    <option value="qt"> qt </option>
+                    <option value="gal"> gal </option>
+                    <option value="mg"> mg </option>
+                    <option value="g"> g </option>
+                    <option value="ml"> ml </option>
+                    <option value="l"> l </option>
+                    <option value="ea"> ea </option>
+                    <option value="to taste"> to taste </option>
+                    <option value="cloves"> cloves </option>
+                    <option value="can"> can </option>
                 </select>
                 <button class="add-button" v-on:click.prevent='addIngredient()'>Add</button>
             </div>
